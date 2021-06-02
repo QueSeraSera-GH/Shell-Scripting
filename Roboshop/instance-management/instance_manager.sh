c38474d4770ed4f04c4fb5b879fafe2947555e49
Q Dev <qdev007@gmail.com>
2021-06-02 19:58:18 -0400

    Adding var.sh

--- Roboshop/instance-management/instance_manager.sh
+++ Roboshop/instance-management/instance_manager.sh
@@ -15,6 +15,6 @@ LVER=2
 
 #aws ec2 run-instances --launch-template LaunchTemplateId=lt-03014931b58e68ec1,Version=2 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]'
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+##aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
 
 
