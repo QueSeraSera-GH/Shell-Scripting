cb2eefb59f07018c140e57c93a4eddab2d6fa009
Q Dev <qdev007@gmail.com>
2021-06-02 19:33:27 -0400

    Adding var.sh

---
+++ Roboshop/instance-management/instance_manager.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+COMPONENT=$1
+
+## -z valdiates the input variable is empty; true if empty
+if [-z "${COMPONENT}"]; then
+    echo "Component input is needed"
+    exit 1
+fi
+
+##manges AWS cli
+LID=lt-03014931b58e68ec1
+LVER=2
+
+
+#aws ec2 run-instances --launch-template LaunchTemplateId=lt-03014931b58e68ec1,Version=2 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]'
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+
+
